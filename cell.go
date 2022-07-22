4dc41b965b7121f3c63e5e0c428f423d0ed7a81d
JeHyuckLee <20171593@edu.hanbat.ac.kr>
2022-07-23 00:35:18 +0900

    7.23

---
+++ cell.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"evsim_golang/definition"
+	"evsim_golang/executor"
+	"evsim_golang/system"
+)
+
+//cell의 원자모델
+type cellout struct {
+	executor *executor.BehaviorModelExecutor
+	msg_list []interface{}
+}
+
+func (m *cellout) Int_trans() {
+	//상태변화
+}
+
+func (m *cellout) Ext_trans(port string, msg *system.SysMessage) {
+	//check 에게 정보를 받음
+}
+
+func (m *cellout) Output() *system.SysMessage {
+	//player 에게 전송
+}
+
+func AC_cellout() *cellout {
+	m := cellout{}
+	m.executor = executor.NewExecutor(0, definition.Infinite, "out", "gosim")
+	m.executor.AbstractModel = &m
+
+	//state
+	m.executor.Behaviormodel.Insert_state("IDLE", definition.Infinite)
+	m.executor.Behaviormodel.Insert_state("OUT", 0)
+	m.executor.Init_state("IDLE")
+
+	//port
+	m.executor.Behaviormodel.CoreModel.Insert_input_port("check")
+	m.executor.Behaviormodel.CoreModel.Insert_output_port("player")
+	return &m
+}
+
+//cell의 원자모델
+type cellin struct {
+	executor *executor.BehaviorModelExecutor
+	msg_list []interface{}
+}
+
+func (m *cellin) Int_trans() {
+	//상태변화
+}
+
+func (m *cellin) Ext_trans(port string, msg *system.SysMessage) {
+	//player가 해당 셀에 왔음
+}
+
+func (m *cellin) Output() *system.SysMessage {
+	//check 에게 출력을 보내서 동작시킴
+}
+
+func AC_cellin() *cellin {
+	m := cellin{}
+	m.executor = executor.NewExecutor(0, definition.Infinite, "in", "gosim")
+	m.executor.AbstractModel = &m
+
+	//state
+	m.executor.Behaviormodel.Insert_state("IDLE", definition.Infinite)
+	m.executor.Behaviormodel.Insert_state("CHECK", 1)
+	m.executor.Init_state("IDLE")
+
+	//port
+	m.executor.Behaviormodel.CoreModel.Insert_input_port("cell")
+	m.executor.Behaviormodel.CoreModel.Insert_output_port("check")
+
+	return &m
+}
+
+//cell의 원자모델
+type check struct {
+	executor *executor.BehaviorModelExecutor
+	msg_list []interface{}
+	block    bool
+}
+
+func (m *check) set_block(b bool) {
+	m.block = b
+}
+func (m *check) get_block() bool {
+	return m.block
+}
+
+func (m *check) Int_trans() {
+	//상태천이
+}
+
+func (m *check) Ext_trans(port string, msg *system.SysMessage) {
+	//in에게 입력을 받으면 check 상태로 가고 연결된 애들에게 입력을 보냄
+	//NEWS 포트로 입력을 받으면 out 상태로 가고 OUT에게 입력을 보냄
+}
+
+func (m *check) Output() *system.SysMessage {
+	//in에게 입력을 받으면 NEWS 포트중 연결된 포트로 출력
+	//NEWS포트 로 입력이 들어오면 입력된 정보를 OuT 에게 전송
+}
+
+func AC_check() *check {
+	//맵 모델
+	m := check{}
+	m.executor = executor.NewExecutor(0, definition.Infinite, "maze", "gosim")
+	m.executor.AbstractModel = &m
+	m.block = false
+
+	//state
+
+	m.executor.Behaviormodel.Insert_state("IDLE", definition.Infinite)
+	//OUT으로부터 입력이 오면 IDLE -> CHECK
+	m.executor.Behaviormodel.Insert_state("CHECK", 0)
+	//NEWS로부터 입력이 오면 IDLE->OUT
+	m.executor.Behaviormodel.Insert_state("OUT", 0)
+
+	//input port
+	m.executor.Behaviormodel.CoreModel.Insert_input_port("north")
+	m.executor.Behaviormodel.CoreModel.Insert_input_port("south")
+	m.executor.Behaviormodel.CoreModel.Insert_input_port("east")
+	m.executor.Behaviormodel.CoreModel.Insert_input_port("west")
+	m.executor.Behaviormodel.CoreModel.Insert_input_port("in")
+	//output port
+	m.executor.Behaviormodel.CoreModel.Insert_output_port("north")
+	m.executor.Behaviormodel.CoreModel.Insert_output_port("south")
+	m.executor.Behaviormodel.CoreModel.Insert_output_port("east")
+	m.executor.Behaviormodel.CoreModel.Insert_output_port("west")
+	m.executor.Behaviormodel.CoreModel.Insert_output_port("out")
+
+	return &m
+}
--- main.go
+++ main.go
@@ -1,23 +1,84 @@
 package main
 
 import (
+	"evsim_golang/definition"
 	"evsim_golang/executor"
-	"evsim_golang/system"
+	"fmt"
 )
 
-type move struct {
-	executor *executor.BehaviorModelExecutor
-	msg_list []interface{}
-}
+//player 는 처음 0.0 에게 입력을 보냄 입력을 받은 0.0은 연결된 0.1, 1.0 에게
+//입력을 보내고 블럭인지 아닌지에 대한 정보를 받아와서 플레이어 에게 알려줌
+//입력을 받은 player 는 어떻게 움직일지 판단한 후에 다음 블럭에게 입력을 보냄
 
-func (m *move) Int_trans() {
+func main() {
+	se := executor.NewSysSimulator()
+	se.Register_engine("gosim", "VIRTURE_TIME", 1)
+	sim := se.Get_engine("gosim")
+	sim.Behaviormodel.CoreModel.Insert_input_port("start")
 
-}
+	//player 결합모델
+	ac_move := AC_move()
+	ac_think := AC_think()
+	sim.Register_entity(ac_move.executor)
+	sim.Register_entity(ac_think.executor)
+	sim.Coupling_relation(nil, "start", ac_move.executor, "start")
+	sim.Coupling_relation(ac_think.executor, "move", ac_move.executor, "think")
 
-func (m *move) Ext_trans(port string, msg *system.SysMessage) {
+	//맵크기
+	width := 100
+	heigth := 100
+	// cell 끼리 연결 을 위해 만든 슬라이스
+	cell := make([][]*executor.BehaviorModelExecutor, heigth)
 
-}
+	for i := 0; i < heigth; i++ {
+		cell[i] = make([]*executor.BehaviorModelExecutor, width)
 
-func (m *move) Output() *system.SysMessage {
+		for j := 0; j < width; j++ {
+			//cell의 원자모델 들 생성
+			ac_check := AC_check()
+			ac_in := AC_cellin()
+			ac_out := AC_cellout()
+			n := fmt.Sprintf("{%n,%n}", j, i)
+			ac_check.executor.Behaviormodel.CoreModel.Set_name(n)
+			ac_out.executor.Behaviormodel.CoreModel.Set_name(n)
+			ac_in.executor.Behaviormodel.CoreModel.Set_name(n)
 
+			//결합모델 cell 만들기
+			sim.Register_entity(ac_check.executor)
+			sim.Register_entity(ac_out.executor)
+			sim.Register_entity(ac_in.executor)
+			sim.Coupling_relation(ac_in.executor, "check", ac_check.executor, "in")
+			sim.Coupling_relation(ac_check.executor, "out", ac_out.executor, "check")
+
+			//player 와 cell 의 연결
+			sim.Coupling_relation(ac_move.executor, "cell", ac_in.executor, "cell")
+			sim.Coupling_relation(ac_out.executor, "player", ac_think.executor, "think")
+			cell[i][j] = ac_check.executor
+		}
+
+	}
+
+	//cell과 cell의 연결
+	for i := 0; i < heigth; i++ {
+		for j := 0; j < width; j++ {
+
+			if i != 0 {
+				sim.Coupling_relation(cell[i][j], "south", cell[i-1][j], "north")
+			}
+			if i != heigth-1 {
+				sim.Coupling_relation(cell[i][j], "north", cell[i+1][j], "south")
+			}
+
+			if j != 0 {
+				sim.Coupling_relation(cell[i][j], "west", cell[i][j-1], "east")
+			}
+			if j != width-1 {
+				sim.Coupling_relation(cell[i][j], "east", cell[i][j+1], "west")
+			}
+		}
+	}
+
+	//시작
+	sim.Insert_external_event("start", nil, 0)
+	sim.Simulate(definition.Infinite)
 }
