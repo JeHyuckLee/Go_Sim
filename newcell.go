5e64efdd1aa3c47100c965c33960694e1de7a563
bskim <skdkwl900@gmail.com>
2022-10-25 03:32:35 +0900

    10.25

--- newcell.go
+++ newcell.go
@@ -5,68 +5,86 @@ import (
 	"evsim_golang/executor"
 	"evsim_golang/system"
 	"fmt"
-	"os"
 )
 
-type cm_cell struct {
-	am_in    *cell_in
-	am_check *cell_check
+//CoopMember
+type cm_coopMember struct {
+	am_seed    *coopMember_seed
+	am_harvest *coopMember_harvest
+	am_ship    *coopMember_ship
 }
 
-func CM_cell(instance_time, destruct_time float64, name, engine_name string, ix, iy int) *cm_cell {
+func CM_coopMember(instance_time, destruct_time float64, name, engine_name string, area int) *cm_coopMember {
 
-	cell := cm_cell{}
-	cell.am_in = AM_cellIn(instance_time, destruct_time, name, engine_name)
-	cell.am_check = AM_cellcheck(instance_time, destruct_time, name, engine_name, ix, iy)
+	cell := cm_coopMember{}
+	cell.am_seed = AM_seed(instance_time, destruct_time, name, engine_name, area)
+	cell.am_harvest = AM_harvest(instance_time, destruct_time, name, engine_name, area)
+	cell.am_ship = AM_ship(instance_time, destruct_time, name, engine_name)
 
 	return &cell
 }
 
-type cell_in struct {
+//Seeding
+type coopMember_seed struct {
 	executor *executor.BehaviorModelExecutor
-	pos      pos
+	area     int
+	harvest  int
+	req_item interface{}
 	msg      *system.SysMessage
 }
 
-func AM_cellIn(instance_time, destruct_time float64, name, engine_name string) *cell_in {
-	m := &cell_in{}
+func AM_seed(instance_time, destruct_time float64, name, engine_name string, area int) *coopMember_seed {
+	m := &coopMember_seed{}
 	m.executor = executor.NewExecutor(instance_time, destruct_time, name, engine_name)
 	m.executor.AbstractModel = m
 
-	//state
+	//infor
+	m.area = area
+	m.harvest = 3
+
+	//statef
 	m.executor.Behaviormodel.Insert_state("IDLE", definition.Infinite)
 	m.executor.Behaviormodel.Insert_state("IN", 0)
 	m.executor.Init_state("IDLE")
 
 	//port
 	m.executor.Behaviormodel.CoreModel.Insert_input_port("in")
-	m.executor.Behaviormodel.CoreModel.Insert_output_port("check")
+	m.executor.Behaviormodel.CoreModel.Insert_output_port("harvest")
 
 	return m
 }
 
-func (m *cell_in) Ext_trans(port string, msg *system.SysMessage) {
-	//player가 해당 셀에 왔음
+func (m *coopMember_seed) Ext_trans(port string, msg *system.SysMessage) {
+	//파종이 필요하다고 요청이 옴
 	if port == "in" {
 		m.executor.Cancel_rescheduling()
 		data := msg.Retrieve()
-		m.pos = data[0].(pos)
+		m.req_item = data[0]
 
 		m.executor.Cur_state = "IN"
 	}
-
 }
 
-func (m *cell_in) Output() *system.SysMessage {
-	//check 에게 출력을 보내서 동작시킴
-	fmt.Println("State: cell in")
-	msg := system.NewSysMessage(m.executor.Behaviormodel.CoreModel.Get_name(), "check")
-	msg.Insert(m.pos)
+func (m *coopMember_seed) Output() *system.SysMessage {
+	//가능한 수확량선에서 필요한 만큼 파종을 함
+	fmt.Println("State: Seeding in")
+	msg := system.NewSysMessage(m.executor.Behaviormodel.CoreModel.Get_name(), "harvest")
 
+	//파종하기
+	req_item := m.req_item.(int)
+	possHarvest := m.area * m.harvest
+	var seeding []int
+	seeding = append(seeding, possHarvest)
+	if possHarvest < req_item {
+		restHarvest := req_item - possHarvest
+		seeding = append(seeding, restHarvest)
+	}
+
+	msg.Insert(seeding)
 	return msg
 }
 
-func (m *cell_in) Int_trans() {
+func (m *coopMember_seed) Int_trans() {
 	//상태변화
 	if m.executor.Cur_state == "IN" {
 		m.executor.Cur_state = "IDLE"
@@ -75,26 +93,20 @@ func (m *cell_in) Int_trans() {
 	}
 }
 
-type cell_check struct {
+//Harvest
+type coopMember_harvest struct {
 	executor *executor.BehaviorModelExecutor
-	cell     [][]int
-	cell2    [][]int
-	pos      pos
-	cellmsg  []cell_msg
+	area     int
 }
 
-func AM_cellcheck(instance_time, destruct_time float64, name, engine_name string, x, y int) *cell_check {
-	m := &cell_check{}
+func AM_harvest(instance_time, destruct_time float64, name, engine_name string, area int) *coopMember_harvest {
+	m := &coopMember_harvest{}
 	m.executor = executor.NewExecutor(instance_time, destruct_time, name, engine_name)
 	m.executor.AbstractModel = m
 
-	m.cellmsg = make([]cell_msg, 4)
-	m.cell = create_map(x, y)
-	m.cell2 = create_map(x, y)
-	m.cellmsg[0].dir = Dir_Nort
-	m.cellmsg[1].dir = Dir_East
-	m.cellmsg[2].dir = Dir_West
-	m.cellmsg[3].dir = Dir_South
+	//infor
+	m.area = area
+
 	//state
 	m.executor.Behaviormodel.Insert_state("IDLE", definition.Infinite)
 	m.executor.Behaviormodel.Insert_state("CHECK", 0)
@@ -107,54 +119,76 @@ func AM_cellcheck(instance_time, destruct_time float64, name, engine_name string
 	return m
 }
 
-func (m *cell_check) Ext_trans(port string, msg *system.SysMessage) {
+func (m *coopMember_harvest) Ext_trans(port string, msg *system.SysMessage) {
 	//player가 해당 셀에 왔음
 	if port == "check" {
-		file1, _ := os.Create("map.csv")
 
 		m.executor.Cancel_rescheduling()
-		data := msg.Retrieve()
-		m.pos = data[0].(pos)
-		m.cell2[m.pos.x][m.pos.y] = +2
-		for i := 0; i < 100; i++ {
-			for j := 0; j < 100; j++ {
-				fmt.Fprint(file1, m.cell2[j][i], ",")
-			}
-			fmt.Fprintln(file1)
-		}
-		file1.Close()
-		fmt.Println("State: cell check")
+
 		m.executor.Cur_state = "CHECK"
 	}
 
 }
 
-func (m *cell_check) Output() *system.SysMessage {
-	//check 에게 출력을 보내서 동작시킴
-	//시작지점을 1.1 로 하고 주변을 벽으로 해야할듯
-	m.cellmsg[0].pos.x = m.pos.x
-	m.cellmsg[1].pos.x = m.pos.x + 1
-	m.cellmsg[2].pos.x = m.pos.x - 1
-	m.cellmsg[3].pos.x = m.pos.x
+func (m *coopMember_harvest) Output() *system.SysMessage {
 
-	m.cellmsg[0].pos.y = m.pos.y - 1
-	m.cellmsg[1].pos.y = m.pos.y
-	m.cellmsg[2].pos.y = m.pos.y
-	m.cellmsg[3].pos.y = m.pos.y + 1
+	return nil
+}
 
-	// 1 = block
-	for i := 0; i < 4; i++ {
-		m.cellmsg[i].block = m.cell[m.cellmsg[i].pos.y][m.cellmsg[i].pos.x]
+func (m *coopMember_harvest) Int_trans() {
+	//상태변화
+	if m.executor.Cur_state == "CHECK" {
+		m.executor.Cur_state = "IDLE"
+	} else {
+		m.executor.Cur_state = "IDLE"
 	}
-	msg := system.NewSysMessage(m.executor.Behaviormodel.CoreModel.Get_name(), "player")
-	msg.Insert(m.cellmsg)
+}
+
+//Shipment
+type coopMember_ship struct {
+	executor *executor.BehaviorModelExecutor
+	msg      *system.SysMessage
+}
+
+func AM_ship(instance_time, destruct_time float64, name, engine_name string) *coopMember_ship {
+	m := &coopMember_ship{}
+	m.executor = executor.NewExecutor(instance_time, destruct_time, name, engine_name)
+	m.executor.AbstractModel = m
+
+	//infor
+
+	//statef
+	m.executor.Behaviormodel.Insert_state("IDLE", definition.Infinite)
+	m.executor.Behaviormodel.Insert_state("IN", 0)
+	m.executor.Init_state("IDLE")
+
+	//port
+	m.executor.Behaviormodel.CoreModel.Insert_input_port("in")
+	m.executor.Behaviormodel.CoreModel.Insert_output_port("check")
+
+	return m
+}
+
+func (m *coopMember_ship) Ext_trans(port string, msg *system.SysMessage) {
+	//player가 해당 셀에 왔음
+	if port == "in" {
+		m.executor.Cancel_rescheduling()
+
+		m.executor.Cur_state = "IN"
+	}
+}
+
+func (m *coopMember_ship) Output() *system.SysMessage {
+	//check 에게 출력을 보내서 동작시킴
+	fmt.Println("State: cell in")
+	msg := system.NewSysMessage(m.executor.Behaviormodel.CoreModel.Get_name(), "check")
 
 	return msg
 }
 
-func (m *cell_check) Int_trans() {
+func (m *coopMember_ship) Int_trans() {
 	//상태변화
-	if m.executor.Cur_state == "CHECK" {
+	if m.executor.Cur_state == "IN" {
 		m.executor.Cur_state = "IDLE"
 	} else {
 		m.executor.Cur_state = "IDLE"
